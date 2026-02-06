41126653b2c33ef7c6398845282594e459d53712
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-06 16:30:16 +0000

    Update create_dimtables.sql

--- scripts/create_dimtables.sql
+++ scripts/create_dimtables.sql
@@ -28,7 +28,6 @@ Script Purpose:
 USE [Forecast]
 GO
 
-/****** Object:  Table [dbo].[tblForecastData_US]    Script Date: 06/02/2026 14:13:14 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -121,4 +120,14 @@ PRIMARY KEY CLUSTERED
 	[Code] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+
+-- Create the dimension table tblUserLogin. Values will be populated manually.
+
+CREATE TABLE [dbo].[Userlogin](
+	[UserLogin] [nvarchar](max) NOT NULL,
+	[ForecastingCustomer] [nvarchar](20) NOT NULL,
+	[TableToAccess] [nvarchar](max) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
