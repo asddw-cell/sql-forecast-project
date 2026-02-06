c813faa102abd9b40a7b80468a3e84475bf14d89
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-06 16:55:15 +0000

    Update create_dimtables.sql

--- scripts/create_dimtables.sql
+++ scripts/create_dimtables.sql
@@ -18,10 +18,11 @@ Script Purpose:
 		tblBrand
 		tblUserLogin
 
-	Region specific:
-		tblForecastCustomer
-		tblItemCategory
-		tblItem
+	Region specific (ensure to add the regional suffix e.g. _AU, _EU, _UK, _US to table names):
+		tblForecastCustomer_UK
+		tblForecastCustomer_US
+		tblItem_UK
+		tblItem_US
 
 */
 
@@ -130,4 +131,64 @@ CREATE TABLE [dbo].[Userlogin](
 	[ForecastingCustomer] [nvarchar](20) NOT NULL,
 	[TableToAccess] [nvarchar](max) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+-- Create the dimension table tblForecastCustomer_UK. Values will be populated via stored procedure.
+
+CREATE TABLE [dbo].[tblForecastCustomer_UK](
+	[Code] [nvarchar](20) NOT NULL,
+	[Description] [nvarchar](50) NOT NULL,
+	[Active] [bit],
+PRIMARY KEY CLUSTERED 
+(
+	[Code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-- Create the dimension table tblForecastCustomer_US. Values will be populated via stored procedure.
+
+CREATE TABLE [dbo].[tblForecastCustomer_US](
+	[Code] [nvarchar](20) NOT NULL,
+	[Description] [nvarchar](50) NOT NULL,
+	[Active] [bit],
+PRIMARY KEY CLUSTERED 
+(
+	[Code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-- Create the dimension table tblItem_UK. Values will be populated on a schedule via stored procedure.
+
+CREATE TABLE [dbo].[tblItem_UK](
+	[ItemNo] [nvarchar](20) NOT NULL,
+	[Item] [nvarchar](123) NOT NULL,
+	[BrandName] [nvarchar](50) NOT NULL,
+	[BrandCode] [nvarchar](25) NOT NULL,
+	[BrandBrand] [nvarchar](175) NOT NULL
+PRIMARY KEY CLUSTERED 
+(
+	[ItemNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-- Create the dimension table tblItem_US. Values will be populated on a schedule via stored procedure.
+
+CREATE TABLE [dbo].[tblItem_US](
+	[ItemNo] [nvarchar](20) NOT NULL,
+	[Item] [nvarchar](123) NOT NULL,
+	[BrandName] [nvarchar](50) NOT NULL,
+	[BrandCode] [nvarchar](25) NOT NULL,
+	[BrandBrand] [nvarchar](175) NOT NULL
+PRIMARY KEY CLUSTERED 
+(
+	[ItemNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
