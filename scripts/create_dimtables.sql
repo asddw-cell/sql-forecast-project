7d82cac78e131d4d486847397c449494e805651e
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-06 16:11:38 +0000

    Update create_dimtables.sql

--- scripts/create_dimtables.sql
+++ scripts/create_dimtables.sql
@@ -58,3 +58,27 @@ INSERT INTO [dbo].[tblForecastType]
 		   (3
            ,'Purchase')
 GO
+
+-- Create the dimension table tblSalesChannel and populate values
+CREATE TABLE [dbo].[tblSalesChannel](
+	[Code] [nvarchar](15) NOT NULL,
+	[Description] [nvarchar](30) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+INSERT INTO [dbo].[tblSalesChannel]
+           ([Code]
+           ,[Description])
+     VALUES
+           ('DOMESTIC'
+           ,'Domestic'),
+		   ('ECOMM'
+           ,'ECommerce'),
+		   ('FOB/DTR'
+           ,'FOB/DI')
+GO
