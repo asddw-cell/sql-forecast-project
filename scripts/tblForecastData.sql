67d432dabd49f965e12b4b9b0e196e169ba2a1a0
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-06 15:40:24 +0000

    Create tblForecastData.sql

---
+++ scripts/tblForecastData.sql
@@ -0,0 +1,84 @@
+/*
+
+==============================================================================
+
+Create transactional ForecastData table
+
+==============================================================================
+
+Script Purpose:
+	This script creates a system versioned table called 'tblForecastData' after checking if it already exists.
+	If the table exists, it is dropped and recreated.
+	*** Note the 3 character suffix after 'tblForecastData' denotes the region e.g. _US, _UK, _EU, _AU
+
+Warning:
+	Running this script will drop the entire 'tblForecastData' table for the entered region if it exists.
+	All data in the table will be permanently deleted. Proceed with caution and ensure you have proper backups 
+	before running this script.
+*/
+
+USE [Forecast]
+GO
+
+/****** Object:  Table [dbo].[tblForecastData_US]    Script Date: 06/02/2026 14:13:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('[dbo].[tblForecastData_US]', 'U') IS NOT NULL
+BEGIN
+	-- Disable system versioning first
+	ALTER TABLE [dbo].[tblForecastData_US]
+		SET (SYSTEM_VERSIONING = OFF);
+
+	DROP TABLE [dbo].[tblForecastData_US];
+	DROP TABLE [dbo].[tblForecastData_USHistory];
+END
+GO
+
+-- Create the transactional Forecast table for US
+CREATE TABLE [dbo].[tblForecastData_US] (
+	[EntryNo] [int] IDENTITY(1, 1) NOT NULL
+	,[ForecastType] [int] NOT NULL
+	,[SalesChannel] [nvarchar](20) NOT NULL
+	,[ForecastCustomer] [nvarchar](20) NOT NULL
+	,[Year] [int] NOT NULL
+	,[MonthNum] [int] NOT NULL
+	,[ItemCategory] [nvarchar](20) NULL
+	,[Brand] [nvarchar](20) NOT NULL
+	,[ItemNo] [nvarchar](20) NOT NULL
+	,[Quantity] [decimal](38, 20) NOT NULL
+	,[Price] [decimal](38, 20) NOT NULL
+	,[PriceType] [int] NOT NULL
+	,[CreatedDate] [datetime] NULL
+	,[CreatedBy] [nvarchar](50) NULL
+	,[ModifiedDate] [datetime] NULL
+	,[ModifiedBy] [nvarchar](50) NULL
+	,[SysStartTime] [datetime2](7) GENERATED ALWAYS AS ROW START HIDDEN NOT NULL
+	,[SysEndTime] [datetime2](7) GENERATED ALWAYS AS ROW END HIDDEN NOT NULL
+	,
+	-- Add primary key to EntryNo
+	PRIMARY KEY CLUSTERED ([EntryNo] ASC) WITH (
+		PAD_INDEX = OFF
+		,STATISTICS_NORECOMPUTE = OFF
+		,IGNORE_DUP_KEY = OFF
+		,ALLOW_ROW_LOCKS = ON
+		,ALLOW_PAGE_LOCKS = ON
+		,OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
+		) ON [PRIMARY]
+	,PERIOD FOR SYSTEM_TIME([SysStartTime], [SysEndTime])
+	) ON [PRIMARY]
+	-- Add history table for recording changes
+	WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[tblForecastData_USHistory]))
+GO
+
+-- Define default versioning start and end dates for rows
+ALTER TABLE [dbo].[tblForecastData_US] ADD DEFAULT(getutcdate())
+FOR [SysStartTime]
+GO
+
+ALTER TABLE [dbo].[tblForecastData_US] ADD DEFAULT(CONVERT([datetime2], '9999 - 12 - 31 23 : 59 : 59.9999999'))
+FOR [SysEndTime]
+GO
