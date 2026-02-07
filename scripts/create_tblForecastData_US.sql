ce78ee7e4223807bbc3ba88342185d2f140a78af
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-07 14:40:29 +0000

    Update create_tblForecastData_US.sql

--- scripts/create_tblForecastData_US.sql
+++ scripts/create_tblForecastData_US.sql
@@ -45,7 +45,7 @@ CREATE TABLE [dbo].[tblForecastData_US] (
 	,[ForecastCustomer] [nvarchar](20) NOT NULL
 	,[Year] [int] NOT NULL
 	,[MonthNum] [int] NOT NULL
-	,[ItemCategory] [nvarchar](20) NULL
+	,[BusinessUnit] [int] NULL
 	,[Brand] [nvarchar](25) NOT NULL
 	,[ItemNo] [nvarchar](20) NOT NULL
 	,[Quantity] [decimal](38, 20) NOT NULL
@@ -76,6 +76,9 @@ CREATE TABLE [dbo].[tblForecastData_US] (
 	-- Add foreign key constraint for ForecastCustomer
 	,CONSTRAINT FK_tblForecastData_US_ForecastCustomer FOREIGN KEY ([ForecastCustomer]) 
 		REFERENCES [dbo].[tblForecastCustomer_US]([Code])
+	-- Add foreign key constraint for BusinessUnit
+	,CONSTRAINT FK_tblForecastData_US_BusinessUnit FOREIGN KEY ([BusinessUnit]) 
+		REFERENCES [dbo].[tblBusinessUnit]([Code])
 	-- Add foreign key constraint for Brand
 	,CONSTRAINT FK_tblForecastData_US_Brand FOREIGN KEY ([Brand]) 
 		REFERENCES [dbo].[tblBrand]([Code])
