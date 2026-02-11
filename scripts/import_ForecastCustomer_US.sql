91981671b6d65fa84ea8ada2bb5683777c7f7805
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-11 18:03:15 +0000

    Update import_ForecastCustomer_US.sql

--- scripts/import_ForecastCustomer_US.sql
+++ scripts/import_ForecastCustomer_US.sql
@@ -8,7 +8,8 @@ Import ForecastCustomer for US
 
 Script Purpose:
 	This script inserts initial data to tblForecastCustomer_US from a SELECT statement against the Forecasting Entity fact table
-    in the US staging database. All customers are set as Active and this should be reviewed.
+    in the US staging database. All customers are set as Active and this should be reviewed. Amazon CA and Walmart CA names are
+	transformed to create unique name values.
 	
 
 */
@@ -22,7 +23,13 @@ INSERT INTO [dbo].[tblForecastCustomer_US] (
 	,[Active]
 	)
 SELECT [Code]
-	,[Name]
+	,CASE
+		WHEN [Code] = 'US_CA_AMA'
+			THEN 'Amazon CA'
+		WHEN [Code] = 'US_CA_WAL'
+			THEN 'Walmart CA'
+		ELSE [Name]
+	END
 	,1
 FROM [NLREPORTING-P].[JetStage_US].[dbo].[DataStatic_dbo_DimForecastingEntity_US]
 WHERE [Code] LIKE 'US_%'
