03f435d3851e416fc98b46b767ba5173c0d5a6e3
asddw-cell <114420504+asddw-cell@users.noreply.github.com>
2026-02-13 11:55:22 +0000

    Create import_ForecastCustomer_UK.sql

---
+++ scripts/import_ForecastCustomer_UK.sql
@@ -0,0 +1,30 @@
+
+/*
+
+==============================================================================
+
+Import ForecastCustomer for UK
+
+==============================================================================
+
+Script Purpose:
+	This script inserts initial data to tblForecastCustomer_UK from a SELECT statement against the Forecasting Entity fact table
+    in the UK staging database. All customers are set as Active and this should be reviewed.
+	
+
+*/
+
+USE [Forecast]
+GO
+
+INSERT INTO [dbo].[tblForecastCustomer_UK] (
+	[Code]
+	,[Description]
+	,[Active]
+	)
+SELECT [Code]
+	,[Name]
+	,1
+FROM [NLREPORTING-P].[JetStage2_UK].[dbo].[DataStatic_dbo_DimForecastingEntity_UK]
+WHERE [Code] LIKE 'UK_%'
+GO
